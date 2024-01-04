3f7fb988733fdbd73e9f24de79add10ba8e08463
Chebichii-Lab <chebichiiserem@gmail.com>
2024-01-04 12:47:18 +0300

    add a while loop and if statement

--- main.py
+++ main.py
@@ -18,5 +18,18 @@ def guess(x):
 
 # the computer guesses the number for a user
 def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
     
+    while feedback != 'c':
+        guess = random.randint(low, high)
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C?? ').lower()
+        if feedback == 'h':
+            high = guess -1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'Yay! The computer guess your number {guess} correctly!')
+
 guess(10)
